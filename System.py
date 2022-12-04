430d058f6a4b32ba48c19137d1e32ddb215aa8b1
Avinash <asdhakne21@gmail.com>
2022-12-05 01:19:42 +0530

    partition information

--- System.py
+++ System.py
@@ -1,2 +1,27 @@
+import psutil
+
 class system:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def partition_type(self):
+        for partition in psutil.disk_partitions():
+            print("Partition: " + partition.mountpoint, "Type: ",
+                  partition.fstype, partition.opts.split(",")[1])
+
+    def partition_memory_status(self):
+        for partition in psutil.disk_partitions():
+            partition_data = psutil.disk_usage(partition.mountpoint)
+            print("Partition: ", partition.mountpoint,
+                  "Total Memory: ", partition_data.total)
+
+            bar = "█" * int(partition_data.percent) + "-" * \
+                (100-int(partition_data.percent))
+
+            print(f"|{bar}| Used: { partition_data.percent }%", end="\n\n")
+
+
+if __name__ == "__main__":
+    sys = system()
+    sys.partition_type()
+    sys.partition_memory_status()
