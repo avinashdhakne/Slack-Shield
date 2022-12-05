5f5fffa5b5da5b3b79996b99d98cb4acb943fe04
Avinash <asdhakne21@gmail.com>
2022-12-05 21:51:19 +0530

    FAT36 upgrade

--- FAT32.py
+++ FAT32.py
@@ -44,63 +44,39 @@ class FAT32(Drive):
         return self.index_of_FAT_FAT32
 
     # Find the first index of root directory i.e. cluster 2
-    def get_index_of_root_directory_FAT32(self):
+    def get_index_of_first_data_cluster_FAT32(self):
         size_of_FAT = self.sectors_per_FAT_FAT32 * \
             self.number_of_FAT * self.bytes_per_sector
 
-        self.index_of_root_directory_FAT32 = self.index_of_FAT_FAT32 + size_of_FAT
-        return self.index_of_root_directory_FAT32
-
-    # Find the first file data cluster i.e. cluster 6
-    def get_index_of_first_data_cluster_FAT32(self):
-
-        # cluster 2
-        size_of_root_directory = self.bytes_per_sector * self.sectors_per_cluster
-
-        # After root directory three clusters are reserved for
-        # - WPS setting i.e cluster 3
-        # - system volume information i.e. cluster 4
-        # - index volume guide i.e. cluster 5
-        size_of_reserved_clusters = (
-            self.bytes_per_sector * self.sectors_per_cluster) * 3
-
-        self.index_of_first_data_cluster_FAT32 = self.index_of_root_directory_FAT32 + \
-            size_of_root_directory + size_of_reserved_clusters
-
-        # First data cluster is cluster 6
+        self.index_of_first_data_cluster_FAT32 = self.index_of_FAT_FAT32 + size_of_FAT
         return self.index_of_first_data_cluster_FAT32
 
     def get_slacked_clusters_FAT32(self):
-
-        starting_index_of_FAT_FAT32 = self.index_of_FAT_FAT32
-        ending_index_of_FAT_FAT32 = self.index_of_FAT_FAT32 + (self.sectors_per_FAT_FAT32
-                                                               * self.bytes_per_sector)
-        self.drive_object.seek(starting_index_of_FAT_FAT32)
+        starting_index_of_FAT = self.index_of_FAT_FAT32
+        ending_index_of_FAT = self.index_of_FAT_FAT32 + (self.sectors_per_FAT_FAT32
+                                                         * self.bytes_per_sector)
+        self.drive_object.seek(starting_index_of_FAT)
         slacked_clusters = []
-        # current_index = self.drive_object.tell()
 
-        # Search FAT for clusters having slack spaces
-        for current_index in tqdm(range(starting_index_of_FAT_FAT32, ending_index_of_FAT_FAT32, 4)):
+        for current_index in tqdm(range(starting_index_of_FAT, ending_index_of_FAT, 4)):
             FAT_entry = self.drive_object.read(4).hex()
 
             # If the cluster is not pointing other cluster
             if(FAT_entry == "ffffff0f"):
                 slacked_cluster_index = int(
-                    (self.drive_object.tell() - starting_index_of_FAT_FAT32)/4 - 1)
+                    (self.drive_object.tell() - starting_index_of_FAT)/4 - 1)
 
-                # If cluster is in data section
-                if(slacked_cluster_index >= 6):
+                # If cluster is in data section as first data cluster i.e. cluster 2 will always contains root directory entries
+                if(slacked_cluster_index > 2):
                     slacked_clusters.append(slacked_cluster_index)
 
-            # current_index =  self.drive_object.tell()
             self.slacked_clusters_FAT32 = slacked_clusters
 
         return self.slacked_clusters_FAT32
 
-    # Fetch the cluster data using cluster index
     def get_cluster_data_FAT32(self, cluster_index):
-        if(cluster_index >= 6):
-            first_data_cluster = 6
+        if(cluster_index > 2):
+            first_data_cluster = 2
             size_of_a_cluster = self.sectors_per_cluster * self.bytes_per_sector
 
             cluster_data_index = self.index_of_first_data_cluster_FAT32 + \
@@ -112,13 +88,17 @@ class FAT32(Drive):
             raise Exception("Trying to fetch system cluster")
         return cluster_data_FAT32
 
-    # Write the clusters having slack spaces in output file
+     # Write the clusters having slack spaces in output file
     def extract_slacked_cluster_FAT32(self):
         slacked_clusters_list = self.slacked_clusters_FAT32
         try:
             file = open("Slack_Spaces.txt", "wb+")
             for cluster_index in slacked_clusters_list:
                 cluster_data = self.get_cluster_data_FAT32(cluster_index)
+
+                file.write(b"\n\n Data from cluster: " + str(cluster_index).encode() + b"\n\n\n")
+                # cluster_header = b"Data from cluster: " + str(cluster_index).encode() + b"\n\n\n"                file.write(cluster_header)
+                # file.write(cluster_header)
                 file.write(cluster_data)
 
         except Exception as e:
@@ -136,8 +116,7 @@ if __name__ == "__main__":
     print(file.get_sectors_per_FAT_FAT32())
     print(file.get_root_cluster_FAT32())
     print(file.get_index_of_FAT_FAT32())
-    print(file.get_index_of_root_directory_FAT32())
     print(file.get_index_of_first_data_cluster_FAT32())
     print(file.get_slacked_clusters_FAT32())
     print(file.extract_slacked_cluster_FAT32())
-    # print(file.get_cluster_data(8))
+    # print(file.get_cluster_data_FAT32(8))
