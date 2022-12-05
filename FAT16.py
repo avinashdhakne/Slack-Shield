51c50b402063e3261dfcd4f6a6513da25847a819
Avinash <asdhakne21@gmail.com>
2022-12-05 10:35:31 +0530

    FAT16 implementing

---
+++ FAT16.py
@@ -0,0 +1,35 @@
+from drive import Drive
+
+
+class FAT16(Drive):
+    def __init__(self, drive) -> None:
+        super().__init__(drive)
+        self.bytes_per_sector = self.get_bytes_per_sector()
+        self.sectors_per_cluster = self.get_sectors_per_cluster()
+        self.number_of_FAT = self.get_number_of_FAT()
+
+    # Get the number of entries possible in root directory
+    def get_root_entries(self):
+        self.drive_object.seek(17)
+        self.root_entries = int.from_bytes(self.drive_object.read(2),"little")
+        return self.root_entries
+
+    # Find the no of sectors of file allocation table
+    def get_sectors_per_FAT(self):
+        self.drive_object.seek(22)
+        self.sectors_per_FAT = int.from_bytes(self.drive_object.read(2),"little")
+        return self.sectors_per_FAT
+
+    # Fine the hidden sectors i.e physical sectors before the drive starts
+    def hidden_sectors(self):
+        self.drive_object.seek(28)
+        self.hidden_sectors = int.from_bytes(self.drive_object.read(4),"little")
+        return self.hidden_sectors
+    
+if __name__ == "__main__":
+    file = FAT16("I")
+    print(file.get_root_entries())
+    print(file.get_sectors_per_FAT())
+    print(file.hidden_sectors())
+
+    
